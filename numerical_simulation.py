04f6b0af8d156c26d5bae30c3d073b22858f214c
swellfish <swellfish92@gmail.com>
2025-04-02 18:29:12 +0900

    # 2025.04.02 # 김세헌
    # 커밋 테스트

--- numerical_simulation.py
+++ numerical_simulation.py
@@ -6,6 +6,7 @@ from CoolProp.HumidAirProp import HAPropsSI
 # from Material_properties import air
 import matplotlib.pyplot as plt
 
+# 아 집에가고싶다
 
 class module_class:
     def __init__(self, var_dict):
